31703a81e41959f53a0287f548681e924bfec4b3
Bob Follek <bfollek@gmail.com>
2021-12-23 14:10:57 -0500

    Added Transpose ATP

--- pkg/funcy_test.go
+++ pkg/funcy_test.go
@@ -11,49 +11,49 @@ import (
 
 func TestFilterWithInts(t *testing.T) {
 	require := require.New(t)
+	input := []int{2, 3, 4, 5, 6, 7, 8, 9}
 	expecting := []int{2, 4, 6, 8}
-	sl := []int{2, 3, 4, 5, 6, 7, 8, 9}
-	result := Filter(sl, func(i int) bool { return i%2 == 0 })
+	result := Filter(input, func(i int) bool { return i%2 == 0 })
 	require.Equal(expecting, result)
 }
 
 func TestFilterWithStrings(t *testing.T) {
 	require := require.New(t)
+	input := []string{"bat", "monkey", "cat", "submarine", "me", "hat", "lunch"}
 	expecting := []string{"bat", "cat", "hat"}
-	sl := []string{"bat", "monkey", "cat", "submarine", "me", "hat", "lunch"}
-	result := Filter(sl, func(s string) bool { return len(s) == 3 })
+	result := Filter(input, func(s string) bool { return len(s) == 3 })
 	require.Equal(expecting, result)
 }
 
 func TestFilterWithEmptySlice(t *testing.T) {
 	require := require.New(t)
+	input := []string{}
 	expecting := []string{}
-	sl := []string{}
-	result := Filter(sl, func(s string) bool { return len(s) == 3 })
+	result := Filter(input, func(s string) bool { return len(s) == 3 })
 	require.Equal(expecting, result)
 }
 
 func TestMapWithInts(t *testing.T) {
 	require := require.New(t)
+	input := []int{2, 3, 4, 5}
 	expecting := []int{4, 6, 8, 10}
-	sl := []int{2, 3, 4, 5}
-	result := Map(sl, func(i int) int { return i * 2 })
+	result := Map(input, func(i int) int { return i * 2 })
 	require.Equal(expecting, result)
 }
 
 func TestMapWithStrings(t *testing.T) {
 	require := require.New(t)
+	input := []string{"BAT", "CaT", "haT"}
 	expecting := []string{"bat", "cat", "hat"}
-	sl := []string{"BAT", "CaT", "haT"}
-	result := Map(sl, strings.ToLower)
+	result := Map(input, strings.ToLower)
 	require.Equal(expecting, result)
 }
 
 func TestMapWithStringsToInts(t *testing.T) {
 	require := require.New(t)
+	input := []string{"1", "3", "5", "7", "9", "11"}
 	expecting := []int{1, 3, 5, 7, 9, 11}
-	sl := []string{"1", "3", "5", "7", "9", "11"}
-	result := Map(sl, func(s string) int {
+	result := Map(input, func(s string) int {
 		i, err := strconv.Atoi(s)
 		if err != nil {
 			log.Fatal(err)
@@ -65,33 +65,33 @@ func TestMapWithStringsToInts(t *testing.T) {
 
 func TestMapWithEmptySlice(t *testing.T) {
 	require := require.New(t)
+	input := []int{}
 	expecting := []int{}
-	sl := []int{}
-	result := Map(sl, func(i int) int { return i * 2 })
+	result := Map(input, func(i int) int { return i * 2 })
 	require.Equal(expecting, result)
 }
 
 func TestReduceAddInts(t *testing.T) {
 	require := require.New(t)
+	input := []int{2, 3, 4, 5}
 	expecting := 14
-	sl := []int{2, 3, 4, 5}
-	result := Reduce(sl, 0, func(i, j int) int { return i + j })
+	result := Reduce(input, 0, func(i, j int) int { return i + j })
 	require.Equal(expecting, result)
 }
 
 func TestReduceAddStrings(t *testing.T) {
 	require := require.New(t)
+	input := []string{"home", "-", "made"}
 	expecting := "home-made"
-	sl := []string{"home", "-", "made"}
-	result := Reduce(sl, "", func(s, t string) string { return s + t })
+	result := Reduce(input, "", func(s, t string) string { return s + t })
 	require.Equal(expecting, result)
 }
 
 func TestReduceCanDoMap(t *testing.T) {
 	require := require.New(t)
+	input := []string{"BAT", "CaT", "haT"}
 	expecting := []string{"bat", "cat", "hat"}
-	sl := []string{"BAT", "CaT", "haT"}
-	result := Reduce(sl, []string{}, func(acc []string, s string) []string {
+	result := Reduce(input, []string{}, func(acc []string, s string) []string {
 		return append(acc, strings.ToLower(s))
 	})
 	require.Equal(expecting, result)
@@ -99,26 +99,26 @@ func TestReduceCanDoMap(t *testing.T) {
 
 func TestReduceWithEmptySlice(t *testing.T) {
 	require := require.New(t)
+	input := []string{}
 	startValue := ""
 	expecting := startValue
-	sl := []string{}
-	result := Reduce(sl, startValue, func(s, t string) string { return s + t })
+	result := Reduce(input, startValue, func(s, t string) string { return s + t })
 	require.Equal(expecting, result)
 }
 
 func TestTransposeSquareInts(t *testing.T) {
 	require := require.New(t)
+	input := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
 	expecting := [][]int{
 		{1, 4, 7},
 		{2, 5, 8},
 		{3, 6, 9},
 	}
-	sl := [][]int{
-		{1, 2, 3},
-		{4, 5, 6},
-		{7, 8, 9},
-	}
-	result := Transpose(sl)
+	result := Transpose(input)
 	require.Equal(expecting, result)
 }
 
